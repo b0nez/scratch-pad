3ae5701db296bd52ca003b3dd5cfb435ba85a1ff
teflonpog <pog@tuta.io>
2016-02-23 01:01:38 +0000

    complete my first pull request

--- in-class-exercises/module-one/my-first-pull-request.js
+++ in-class-exercises/module-one/my-first-pull-request.js
@@ -17,6 +17,7 @@
  * 
  *   mocha -R spec --grep my-first-pull-request
  * 
+ * Anything
  */
 
 /** 
@@ -24,7 +25,7 @@
  */
 function print(value) {
   // YOUR CODE BELOW HERE //
-  console.log(value);
+ console.log(value); 
   
   
   
